7aa9aa8f3349d460a5f4269660bb1bf7de54a1ca
hemanth sattenapalli <saihemanth.s@outlook.com>
2023-07-02 19:56:08 +0530

    Adding camera behaviour

---
+++ Assets/Scripts/FactionScore.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+
+public class FactionScore : MonoBehaviour
+{
+    [SerializeField] string m_factionName;
+    [SerializeField] CoinType m_coinType;
+    [SerializeField] Color FactionColor;
+    [SerializeField] TextMeshProUGUI m_factionNameText;
+    [SerializeField] Transform m_coinHolder;
+    [SerializeField] GameObject m_coinGameObject;
+    [SerializeField] TextMeshProUGUI m_scoreText;
+    List<GameObject> m_coins = new List<GameObject>();
+    int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_factionNameText.text = m_factionName;
+        GameController.Instance.OnFactionScored += Instance_OnFactionScored;
+    }
+
+    private void Instance_OnFactionScored(CoinType faction, int _score)
+    {
+        if(faction == m_coinType)
+        {
+            score += _score;
+            if(_score > 0)
+            {
+                var k = Instantiate(m_coinGameObject, m_coinHolder);
+                k.GetComponent<Image>().color = FactionColor;
+                m_coins.Add(k);
+            }
+            else
+            {
+                Destroy(m_coins.First());
+            }
+            m_scoreText.text = "Score : " + score.ToString();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -21,6 +22,9 @@ public class GameController : MonoBehaviour
     
     public RuleEvaluator RuleEvaluator { get { return ruleEvaluator; } }
     public GameManager GameManager { get {  return gameManager; } }
+
+    public event Action<CoinType, int> OnFactionScored;
+    public event Action ResetPhysicsCoins;
     public void RegisterGameManager(GameManager manager)
     {
         gameManager = manager;
@@ -28,5 +32,13 @@ public class GameController : MonoBehaviour
     public void RegisterRuleEvaluator(RuleEvaluator Evaluator)
     {
         ruleEvaluator = Evaluator;
+    }
+    public void InvokeScoreEvent(CoinType faction, int value)
+    {
+        OnFactionScored?.Invoke(faction, value);
+    }
+    public void InvokeResetCoinPhysics()
+    {
+        ResetPhysicsCoins?.Invoke();
     }
 }
